07eb0c19a4854a30c546c274d0e3ab06e23caee4
Donnahue George <Donnahuegjr@gmail.com>
2020-11-18 21:29:37 -0500

    typo in variables

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ var upperCase = "A B C D E F G H I J K L M N O P Q R S T U V W X Y Z";
 var upperCaseArr = upperCase.split(" ");
 var numbers = "0 1 2 3 4 5 6 7 8 9";
 var numbersArr = numbers.split(" ");
-var special = "1 @ # $ % ^ & * ( ) - _ + = | [ ] { } ~ ` < > ? ; '";
+var special = "! @ # $ % ^ & * ( ) - _ + = | [ ] { } ~ ` < > ? ; '";
 var specialArr = special.split(" ");
 var allChar = [];
 var result = "";
